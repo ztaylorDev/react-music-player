50fbe558edc203b1f2427e4ca94f091f61934e7c
asiaromaria <designsbyarc@gmail.com>
2021-07-29 18:23:10 -0700

    added filter method and onChange.

--- react-music-player/src/App.js
+++ react-music-player/src/App.js
@@ -2,37 +2,39 @@ import React, { Component } from "react";
 import axios from "axios";
 import Song from "./Components/Song";
 import "./App.css";
-//import NavBar from './Navbar';
-import SearchBar from "./Components/SearchBar";
+// import NavBar from './Navbar';
+// import SearchBar from "./Components/SearchBar";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
+      userInput: "",
       songs: [{ title: "", artist: "", genre: "", album: "", releaseDate: "" }],
     };
   }
 
-  goToNextSong() {
-    let tempSongNumber = this.state.props;
-    tempSongNumber++;
-    if (tempSongNumber === this.Song.length) {
-      tempSongNumber = 0;
-    }
-    this.setState({
-      songNumber: tempSongNumber,
-    });
-  }
-  goToPreviousSong() {
-    let tempSongNumber = this.state.props;
-    tempSongNumber--;
-    if (tempSongNumber < 0) {
-      tempSongNumber = this.Song.length;
-    }
-    this.setState({
-      songNumber: tempSongNumber,
-    });
-  }
+  // goToNextSong() {
+  //   let tempSongNumber = this.state.props;
+  //   tempSongNumber++;
+  //   if (tempSongNumber === this.Song.length) {
+  //     tempSongNumber = 0;
+  //   }
+  //   this.setState({
+  //     songNumber: tempSongNumber,
+  //   });
+  // }
+  // goToPreviousSong() {
+  //   let tempSongNumber = this.state.props;
+  //   tempSongNumber--;
+  //   if (tempSongNumber < 0) {
+  //     tempSongNumber = this.Song.length;
+  //   }
+  //   this.setState({
+  //     songNumber: tempSongNumber,
+  //   });
+  // }
 
   componentDidMount() {
     console.log("i mounted");
@@ -46,25 +48,43 @@ class App extends Component {
         "http://www.devcodecampmusiclibrary.com/api/music/"
       );
       console.log(response.data);
-      this.setState( 
-          { 
-              songs: response.data 
-            }
-          );
+      this.setState({
+        songs: response.data,
+      });
     } catch (err) {
       console.log("Error in API call!", err);
     }
   }
 
+  filterList() {
+    const filteredList = this.state.songs.filter(() => {
+      if (this.state.userInput === "The Beatles") {
+        return(true)
+      }
+      else {
+        return false
+      }
+    });
+    return filteredList
+  }
+
+  handleChange(e) {
+    this.setState({ userInput: e.target.value });
+    console.log(this.state.userInput);
+    console.log(this.filterList());
+  }
+
   render() {
+    const userInput = this.state.userInput;
     return (
       <div>
+        <fieldset>
+          <legend>Search by Song:</legend>
+          <input value={userInput} onChange={this.handleChange} />
+        </fieldset>
         {/* <SearchBar /> */}
         <Song songs={this.state.songs} />
-        {/* <Song songs={this.songs[this.state.songNumber]} nextSong={() => this.goToNextSong()}  */}
-        {/* previousSong={() => this.goToPreviousSong()}/>  */}
-        <button onClick={this.makeGetRequest}>Previous track</button>
-        <button className="nextbutton">Next Track</button>
+        {this.makeGetRequest}
       </div>
     );
   }

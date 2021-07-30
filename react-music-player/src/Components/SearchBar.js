21c55bbf77be87e974e64e81a95a743c7da985ef
asiaromaria <designsbyarc@gmail.com>
2021-07-29 18:23:38 -0700

    created searchbar.

--- react-music-player/src/Components/SearchBar.js
+++ react-music-player/src/Components/SearchBar.js
@@ -1,29 +1,22 @@
 import React from "react";
-import Song from "./Song";
 
-const SearchBar = ({ props, setKeyword }) => {
-  const BarStyling = {
-    width: "20rem",
-    background: "#F2F1F9",
-    border: "none",
-    padding: "0.5rem",
-  };
-  return (
-      <div>
-          {props.songs.filter((song, index) => (
-              <span> {song.genre} </span>
-          ))}
-    <input
-      style={BarStyling}
-      key="random1"
-      value={song.genre}
-      placeholder={"search"}
-      onChange={(e) => setKeyword(e.target.value)}
-    />
-    </div>
-  );
-    
+const SearchBar = (props) => {
+    return(
+        
+    <form action="/" method="get">
+        <label htmlFor="header-search">
+        <span className="visually-hidden"></span>
+        </label>
+        <input
+        type="text"
+        id="header-search"
+        placeholder="search songs"
+        name="s"
+        value={userInput} onChange={this.handleChange}
+        />
+        <button type="submit">Search</button>
+    </form>
+    );
 };
-
 
 export default SearchBar;
